be951daff624c614fea6b3fda22b9ee08f699372
Matt Garrish <matt.garrish@gmail.com>
2017-05-08 11:23:09 -0400

    Removing old IDPF build process

--- util/tidy/tidy.bat
+++
@@ -1,2 +0,0 @@
-for /f "tokens=*" %%a in ('dir /b /s *.html') do tidy --doctype html5 --output-html yes --char-encoding utf8 --newline lf --clean yes --indent auto --tidy-mark no --wrap 0 --write-back yes "%%a" | echo file: %%a
- 
