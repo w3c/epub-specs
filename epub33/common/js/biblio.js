7d32a27b4f663563dfd68ed21bab757be39bbfdf
Matt Garrish <mattgarrish@users.noreply.github.com>
2021-01-05 13:38:04 -0400

    relative link fixes

--- epub33/common/js/biblio.js
+++ epub33/common/js/biblio.js
@@ -440,20 +440,10 @@ var biblio = {
 		"href": "https://www.w3.org/TR/epub-rs-33/",
 		"publisher": "W3C"
 	},
-	"EPUB-CHANGES-33": {
-		"authors":[
-			"Matt Garrish",
-			"Dave Cramer"
-		],
-		"title": "EPUB Changes 3.3",
-		"href": "https://www.w3.org/TR/epub-changes-33/",
-		"publisher": "W3C"
-	},
 	"EPUB-OVERVIEW-33": {
 		"authors":[
     		"Garth Conboy",
     		"Matt Garrish",
-    		"MURATA Makoto",
     		"Daniel Weck"],
 		"title": "EPUB 3 Overview",
 		"href": "https://www.w3.org/TR/epub-overview-33/",
