f5f73923e6002d2ed0e0866c78cdfebe08efd7f8
Matt Garrish <mattgarrish@users.noreply.github.com>
2021-09-28 08:50:53 -0300

    fixes to avoid null errors

--- epub33/common/js/permalink-a11y.js
+++ epub33/common/js/permalink-a11y.js
@@ -9,18 +9,44 @@ function updatePermaLinks() {
         var secno = '';
         var sectype = '';
         
-        if (heading.tagName.toLowerCase() == 'div') {
-        	// permalink for an example
-        	// number is inside the a tag with no class
-        	secno = links[i].querySelector('bdi').textContent;
-        	sectype = 'Example ';
-        }
+        switch (heading.tagName.toLowerCase()) {
         
-        else {
-        	// number for sections/appendixes is sibling of the a tag with secno class
-	        secno = heading.querySelector('bdi.secno').textContent;
-	        // determine if section/appendix from parent class
-	        sectype = heading.parentNode.classList.contains('appendix') ? 'Appendix ' : 'Section ';
+	        case 'span':
+	        	// permalink for idl
+	        	
+	        	// use link text for name
+	        	secno = links[i].textContent;
+	        	sectype = '';
+	        	break;
+	        
+	        case 'div':
+	        	// permalink for an example
+	        	
+	        	// avoid the permalink references in the hidden dfn-panel respec generates
+	        	if (links[i].closest('div.dfn-panel')) {
+	        		break;
+	        	}
+	        	
+	        	// number is inside the a tag with no class
+	        	secno = links[i].querySelector('bdi');
+	        	if (secno == null) {
+	        		console.log('Error creating new permalink for :' + parent.textContent);
+	        	}
+	        	secno = secno.textContent
+	        	sectype = 'Example ';
+	        	break;
+	        
+	        default:
+	        	// number for sections/appendixes is sibling of the a tag with secno class
+		        secno = heading.querySelector('.secno');
+	        	if (secno == null) {
+	        		console.log('Error creating new permalink for :' + parent.textContent);
+	        		break;
+	        	}
+	        	secno = secno.textContent
+		        // determine if section/appendix from parent class
+		        sectype = heading.parentNode.classList.contains('appendix') ? 'Appendix ' : 'Section ';
+		        break;
         }
         
         
