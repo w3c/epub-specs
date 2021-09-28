7ae929a34605a4e15c9ab0135ecaf7340a5b180b
Matt Garrish <mattgarrish@users.noreply.github.com>
2021-09-28 07:01:49 -0300

    refine code to grab the section number for permalink name instead of the full number and title

--- epub33/common/js/permalink-a11y.js
+++ epub33/common/js/permalink-a11y.js
@@ -4,9 +4,27 @@ function updatePermaLinks() {
     
     for (var i = 0; i < links.length; i++) {
         
-        var heading = links[i].parentNode.textContent;
+        var heading = links[i].parentNode;
         
-        links[i].setAttribute('aria-label', 'Permalink for ' + heading);
-        links[i].setAttribute('title', 'Permalink for ' + heading);
+        var secno = '';
+        var sectype = '';
+        
+        if (heading.tagName.toLowerCase() == 'div') {
+        	// permalink for an example
+        	// number is inside the a tag with no class
+        	secno = links[i].querySelector('bdi').textContent;
+        	sectype = 'Example ';
+        }
+        
+        else {
+        	// number for sections/appendixes is sibling of the a tag with secno class
+	        secno = heading.querySelector('bdi.secno').textContent;
+	        // determine if section/appendix from parent class
+	        sectype = heading.parentNode.classList.contains('appendix') ? 'Appendix ' : 'Section ';
+        }
+        
+        
+        links[i].setAttribute('aria-label', 'Permalink for ' + sectype + secno);
+        links[i].setAttribute('title', 'Permalink for ' + sectype + secno);
     }
 }
