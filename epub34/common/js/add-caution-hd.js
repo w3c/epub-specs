99bdcc39608b0d1bf7951825970b5aaa1bf0c7ac
Matt Garrish <mattgarrish@users.noreply.github.com>
2025-02-12 13:45:20 -0500

    create epub 3.4 folder with baseline documents; fix github issue history links in 3.3 docs

---
+++ epub34/common/js/add-caution-hd.js
@@ -0,0 +1,29 @@
+
+function addCautionHeaders() {
+	var cautions = document.querySelectorAll('div.caution');
+	
+	for (var i = 0; i < cautions.length; i++) {
+		
+		cautions[i].setAttribute('id', 'caution-'+i);
+		
+		// find current parent heading level for setting aria-level on caution heading
+		var parent_section = cautions[i].closest('section');
+		var parent_hd = parent_section.querySelector('h2,h3,h4,h5,h6');
+		var parent_num = parent_hd.localName.replace('h','');
+		
+		var aria_level = Number(parent_num) + 1;
+		
+		if (aria_level > 6) { aria_level = 6; }
+		
+		var caution_hd = document.createElement('div');
+			caution_hd.setAttribute('role', 'heading');
+			caution_hd.setAttribute('aria-level', aria_level);
+			caution_hd.setAttribute('class','caution-title marker');
+		
+		var caution_label = document.createElement('span');
+			caution_label.appendChild(document.createTextNode('Caution'));
+		
+		caution_hd.appendChild(caution_label);
+		cautions[i].prepend(caution_hd);
+	} 
+}
